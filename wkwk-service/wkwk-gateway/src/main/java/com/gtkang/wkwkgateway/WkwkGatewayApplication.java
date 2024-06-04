de434bdb5e565932363afeb75becea2d575b664b
turkey <1281314239@qq.com>
2024-06-05 00:17:25 +0800

    feign和gateway框架

---
+++ wkwk-service/wkwk-gateway/src/main/java/com/gtkang/wkwkgateway/WkwkGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.gtkang.wkwkgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WkwkGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WkwkGatewayApplication.class, args);
+    }
+
+}
